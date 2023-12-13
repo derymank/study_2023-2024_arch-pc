54e043aef589edfb73d8af014df8d9f20b4eb209
Darya Mankovskaya <1132236038@rudn.ru>
2023-12-13 15:23:15 +0300

    Add existing files

---
+++ labs/lab05/lab5-2-1.asm
@@ -0,0 +1,21 @@
+;------------------------------------------------------------------
+; Программа вывода сообщения на экран и ввода строки с клавиатуры
+;------------------------------------------------------------------;------------------- Объявление переменных ----------------
+%include 'in_out.asm'
+SECTION .data
+msg: DB 'Введите строку:',0h
+SECTION .bss
+buf1: RESB 80
+SECTION .text
+GLOBAL _start 
+_start:
+mov eax, msg
+call sprint
+mov ecx, buf1
+mov edx, 80
+call sread
+mov eax, 4
+mov ebx, 1
+mov ecx,buf1 
+int 80h
+call quit
